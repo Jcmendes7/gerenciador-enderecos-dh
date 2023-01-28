dbfa0a18678a2453c2f247fd8aa77db9a3760e93
Jcmendes7 <josecarlosms7f@gmail.com>
2023-01-28 17:50:14 -0300

    estilzação do input password, useState adicionado

--- src/pages/UserRegistrationForm/index.jsx
+++ src/pages/UserRegistrationForm/index.jsx
@@ -1,15 +1,40 @@
-import { EnvelopeSimple, Eye, LockSimple, Play, User } from "phosphor-react";
+import {
+  EnvelopeSimple,
+  LockSimple,
+  Play,
+  User,
+} from "phosphor-react";
+
+import {eyeOff} from 'react-icons-kit/feather/eyeOff'
+import {eye} from 'react-icons-kit/feather/eye'
+
+
+import { Icon } from 'react-icons-kit'
+import { useState } from "react";
+
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header/Header";
 
 import styles from "./UserRegistrationForm.module.css";
 
 export function UserRegistrationForm() {
+  const [ type, setType] = useState('password');
+  const [ iconEye, setIconEye ] = useState(eyeOff)
   function handleSubmit(event) {
     event.preventDefault();
   }
 
   function handleOnChange(event) {}
+
+  function handleOnClickEye() {
+    if(type === "password") {
+      setIconEye(eye);
+      setType('text')
+    } else {
+      setIconEye(eyeOff)
+      setType('password')
+    }
+  }
 
   return (
     <>
@@ -52,11 +77,13 @@ export function UserRegistrationForm() {
                   <LockSimple size={24} />{" "}
                 </span>
                 <input
-                  type="password"
+                  type={type}
                   id="password"
                   placeholder="Digite uma senha"
                 />
-                <a> <Eye size={22} /> </a>
+                <a onClick={handleOnClickEye}>
+                  <Icon icon={iconEye} size={18} />
+                </a>
               </div>
             </div>
 
