82d621cd4fd30098f9479036b80f5947f6167495
Jcmendes7 <josecarlosms7f@gmail.com>
2023-01-29 09:27:06 -0300

    logo png  retirado do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import styles from './Header.module.css'
 import { List } from 'phosphor-react'
-import logoHeader from '../../assets/logo.png'
 
 export function Header({ title }) {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img src={logoHeader} alt="" />
         <strong>{ title }</strong>
       </div>
       <button className={styles.listMenu}>
